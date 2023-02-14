1d851ffcc67e6c72e09c9ae2caf142820c0886e5
user <kupaev.itrum@gmail.com>
2023-02-14 15:17:56 +0300

    Auth done

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import AuthForm from '../AuthForm/AuthForm';
 import { AuthEnum } from '../../interfaces/AuthEnum';
@@ -6,21 +6,35 @@ import HospitalImage from './../../assets/images/Hospital.svg';
 
 import styles from './Auth.module.scss';
 
-import type { FC } from 'react';
 import type { IAuthProps } from '../../interfaces/propTypes/IAuthProps';
 
-const Auth: FC<IAuthProps> = ({ setTitleBody }): JSX.Element => {
-  const [renderType, setRenderType] = useState<AuthEnum>(AuthEnum.register);
-  return (
-    <div className={styles.auth}>
-      <div className={styles.authContainer}>
-        <div className={styles.authImage}>
-          <img src={HospitalImage} alt="hospital" />
+class Auth extends React.Component<IAuthProps, { renderType: AuthEnum }> {
+  constructor (props: IAuthProps) {
+    super(props);
+    this.setRenderType = this.setRenderType.bind(this);
+    this.state = {
+      renderType: AuthEnum.register
+    };
+  }
+
+  setRenderType (value: AuthEnum): void {
+    this.setState({
+      renderType: value
+    });
+  }
+
+  render (): JSX.Element {
+    return (
+      <div className={styles.auth}>
+        <div className={styles.authContainer}>
+          <div className={styles.authImage}>
+            <img src={HospitalImage} alt="hospital" />
+          </div>
+          <AuthForm setTitleBody={this.props.setTitleBody} renderType={this.state.renderType} setRenderType={this.setRenderType} />
         </div>
-        <AuthForm setTitleBody={setTitleBody} renderType={renderType} setRenderType={setRenderType} />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Auth;
