f90c08fff3bc2def95a9ad929954690b7d85af6b
user <kupaev.itrum@gmail.com>
2023-02-15 14:39:22 +0300

    ListItem done

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -5,36 +5,37 @@ import edit from './../../assets/images/edit.svg';
 
 import styles from './ListItem.module.scss';
 
-import type { FC } from 'react';
 import type { IAdmission } from '../../interfaces/IAdmission';
 
 export interface IListitemProps {
   item: IAdmission
 }
 
-const ListItem: FC<IListitemProps> = ({ item }): JSX.Element => {
-  return (
-    <div className={styles.admissions} key={item._id}>
-      <div className={styles.pacient}><span>{item.pacient}</span></div>
-      <div className={styles.doctor}><span>{item.doctor}</span></div>
-      <div className={styles.date}><span>{item.date}</span></div>
-      <div className={styles.complaint}><span>{item.complaint}</span></div>
-      <div className={styles.buttons}>
-        <span
-          id={item._id}
-          className={styles.remove}
-        >
-          <img src={remove} alt="remove" />
-        </span>
-        <span
-          id={item._id}
-          className={styles.edit}
-        >
-          <img src={edit} alt="edit" />
-        </span>
+class ListItem extends React.Component<IListitemProps> {
+  render (): JSX.Element {
+    return (
+      <div className={styles.admissions} key={this.props.item._id}>
+        <div className={styles.pacient}><span>{this.props.item.pacient}</span></div>
+        <div className={styles.doctor}><span>{this.props.item.doctor}</span></div>
+        <div className={styles.date}><span>{this.props.item.date}</span></div>
+        <div className={styles.complaint}><span>{this.props.item.complaint}</span></div>
+        <div className={styles.buttons}>
+          <span
+            id={this.props.item._id}
+            className={styles.remove}
+          >
+            <img src={remove} alt="remove" />
+          </span>
+          <span
+            id={this.props.item._id}
+            className={styles.edit}
+          >
+            <img src={edit} alt="edit" />
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ListItem;
