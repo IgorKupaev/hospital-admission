7c35588199b5a6565fe4b99f63e059d08f0fb416
user <kupaev.itrum@gmail.com>
2023-02-15 14:26:51 +0300

    FormErrors done

--- src/components/FormErrors/FormErrors.tsx
+++ src/components/FormErrors/FormErrors.tsx
@@ -5,21 +5,32 @@ import { useErrors } from '../../hooks/useErrors';
 import styles from './FormErrors.module.scss';
 
 import type { IFormErrorsProps } from '../../interfaces/propTypes/IFormErrorsProps';
-import type { FC } from 'react';
 import type { IUseErrors } from '../../interfaces/IUseErrors';
 
-const FormErrors: FC<IFormErrorsProps> = ({ password, login, confirmError }): JSX.Element => {
-  const { loginLength, passwordEmpty, latinAndDigit, passwordLength, loginEmpty }: IUseErrors = useErrors(password, login);
-  return (
-    <div>
-      {passwordEmpty && <span className={styles.err}>Укажите пароль</span>}
-      {latinAndDigit && <span className={styles.err}>Логин и пароль должны содержать цифры и латинские буквы</span>}
-      {passwordLength && <span className={styles.err}>Пароль должен содержать хотя бы 6 символов</span>}
-      {loginEmpty && <span className={styles.err}>Укажите логин</span>}
-      {loginLength && <span className={styles.err}>Логин должен содержать хотя бы 6 символов</span>}
-      {confirmError && <span className={styles.err}>Подтвердите пароль</span>}
-    </div>
-  );
-};
+interface FormErrorsState {
+  conditions: IUseErrors
+}
+class FormErrors extends React.Component<IFormErrorsProps, FormErrorsState> {
+  constructor (props: IFormErrorsProps) {
+    super(props);
+
+    this.state = {
+      conditions: useErrors(this.props.password, this.props.login)
+    };
+  }
+
+  render (): JSX.Element {
+    return (
+      <div>
+        {this.state.conditions.passwordEmpty && <span className={styles.err}>Укажите пароль</span>}
+        {this.state.conditions.latinAndDigit && <span className={styles.err}>Логин и пароль должны содержать цифры и латинские буквы</span>}
+        {this.state.conditions.passwordLength && <span className={styles.err}>Пароль должен содержать хотя бы 6 символов</span>}
+        {this.state.conditions.loginEmpty && <span className={styles.err}>Укажите логин</span>}
+        {this.state.conditions.loginLength && <span className={styles.err}>Логин должен содержать хотя бы 6 символов</span>}
+        {this.props.confirmError && <span className={styles.err}>Подтвердите пароль</span>}
+      </div>
+    );
+  }
+}
 
 export default FormErrors;
