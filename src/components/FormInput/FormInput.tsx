fc87b14a4bf96a6924dcc16f8560eebb8c5ee608
user <kupaev.itrum@gmail.com>
2023-02-15 14:32:59 +0300

    FormInput done

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import styles from './FormInput.module.scss';
 
 import type { IFormInputProps } from '../../interfaces/propTypes/IFormInputProps';
-import type { FC } from 'react';
 
-const FormInput: FC<IFormInputProps> = ({ title, ...props }): JSX.Element => {
-  return (
-    <div className={styles.inputContainer}>
-      <h4 className={styles.title}>{title}:</h4>
-      <input className={styles.input} {...props} placeholder={title} />
-    </div>
-  );
-};
+class FormInput extends React.Component<IFormInputProps> {
+  render (): JSX.Element {
+    return (
+      <div className={styles.inputContainer}>
+        <h4 className={styles.title}>{this.props.title}:</h4>
+        <input className={styles.input} {...this.props} placeholder={this.props.title} />
+      </div>
+    );
+  }
+}
 
 export default FormInput;
