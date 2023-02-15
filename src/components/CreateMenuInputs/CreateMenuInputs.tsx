99b4cfa8685edd779ac2c6e280f2705c95bb6200
user <kupaev.itrum@gmail.com>
2023-02-15 13:58:37 +0300

    CreateMenuInputs done

--- src/components/CreateMenuInputs/CreateMenuInputs.tsx
+++ src/components/CreateMenuInputs/CreateMenuInputs.tsx
@@ -2,59 +2,73 @@ import React from 'react';
 
 import styles from './CreateMenuInputs.module.scss';
 
-import type { FC, ChangeEvent } from 'react';
+import type { ChangeEvent } from 'react';
 
 import type { ICreateMenuInputsProps } from '../../interfaces/propTypes/ICreateMenuInputsProps';
 
-const CreateMenuInputs: FC<ICreateMenuInputsProps> = ({ newAdmission, setNewAdmission }): JSX.Element => {
-  const setNewPacient = (e: ChangeEvent<HTMLInputElement>): void => {
-    setNewAdmission({ ...newAdmission, pacient: e.target.value });
+class CreateMenuInputs extends React.Component<ICreateMenuInputsProps> {
+  constructor (props: ICreateMenuInputsProps) {
+    super(props);
+    this.setNewPacient = this.setNewPacient.bind(this);
+    this.setNewDoctor = this.setNewDoctor.bind(this);
+    this.setNewDate = this.setNewDate.bind(this);
+    this.setNewComplaint = this.setNewComplaint.bind(this);
+  }
+
+  setNewPacient (e: ChangeEvent<HTMLInputElement>): void {
+    this.props.setNewAdmission({ ...this.props.newAdmission, pacient: e.target.value });
   };
-  const setNewDoctor = (e: ChangeEvent<HTMLInputElement>): void => {
-    setNewAdmission({ ...newAdmission, doctor: e.target.value });
+
+  setNewDoctor (e: ChangeEvent<HTMLInputElement>): void {
+    this.props.setNewAdmission({ ...this.props.newAdmission, doctor: e.target.value });
   };
-  const setNewDate = (e: ChangeEvent<HTMLInputElement>): void => {
-    setNewAdmission({ ...newAdmission, date: e.target.value });
+
+  setNewDate (e: ChangeEvent<HTMLInputElement>): void {
+    this.props.setNewAdmission({ ...this.props.newAdmission, date: e.target.value });
   };
-  const setNewComplaint = (e: ChangeEvent<HTMLInputElement>): void => {
-    setNewAdmission({ ...newAdmission, complaint: e.target.value });
+
+  setNewComplaint (e: ChangeEvent<HTMLInputElement>): void {
+    this.props.setNewAdmission({ ...this.props.newAdmission, complaint: e.target.value });
   };
-  return (
-    <>
-      <div className={styles.menuInput}>
-        <span>Имя: </span>
-        <input
-          value={newAdmission.pacient}
-          onChange={setNewPacient}
-          type="text"
-        />
-      </div>
-      <div className={styles.menuInput}>
-        <span>Врач: </span>
-        <input
-          value={newAdmission.doctor}
-          onChange={setNewDoctor}
-          type="text"
-        />
-      </div>
-      <div className={styles.menuInput}>
-        <span>Дата: </span>
-        <input
-          value={newAdmission.date}
-          onChange={setNewDate}
-          type="date"
-        />
-      </div>
-      <div className={styles.menuInput}>
-        <span>Жалобы: </span>
-        <input
-          value={newAdmission.complaint}
-          onChange={setNewComplaint}
-          type="text"
-        />
-      </div>
-    </>
-  );
-};
+
+  render (): JSX.Element {
+    return (
+      <>
+        <div className={styles.menuInput}>
+          <span>Имя: </span>
+          <input
+            value={this.props.newAdmission.pacient}
+            onChange={this.setNewPacient}
+            type="text"
+          />
+        </div>
+        <div className={styles.menuInput}>
+          <span>Врач: </span>
+          <input
+            value={this.props.newAdmission.doctor}
+            onChange={this.setNewDoctor}
+            type="text"
+          />
+        </div>
+        <div className={styles.menuInput}>
+          <span>Дата: </span>
+          <input
+            value={this.props.newAdmission.date}
+            onChange={this.setNewDate}
+            type="date"
+          />
+        </div>
+        <div className={styles.menuInput}>
+          <span>Жалобы: </span>
+          <input
+            value={this.props.newAdmission.complaint}
+            onChange={this.setNewComplaint}
+            type="text"
+          />
+        </div>
+      </>
+    );
+  }
+}
 
 export default CreateMenuInputs;
