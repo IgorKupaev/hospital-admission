38d925dd6c8c5c56634300ccfe7469d0414a0a0a
user <kupaev.itrum@gmail.com>
2023-02-15 14:19:21 +0300

    FilterMenu done

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,59 +1,83 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import remove from './../../assets/images/remove.svg';
-import { useAppDispatch } from '../../hooks/redux';
 import { changeFilter, clearFilter } from '../../store/reducers/filterSlice';
 
 import styles from './FilterMenu.module.scss';
 
-import type { FC } from 'react';
 import type { IFilterOptions } from '../../interfaces/IFilterOptions';
 import type { IFilterMenuProps } from '../../interfaces/propTypes/IFilterMenuProps';
+import { store } from '../../store/store';
 
-const FilterMenu: FC<IFilterMenuProps> = ({ isFilterHidden, setIsFilterHidden }): JSX.Element => {
-  const initialFilter: IFilterOptions = { from: '', to: '' };
-  const [filter, setFilter] = useState<IFilterOptions>(initialFilter);
-  const dispatch = useAppDispatch();
+interface IFilterMenuState {
+  filter: IFilterOptions
+}
 
-  const filterButtonHandler = (): void => {
-    dispatch(changeFilter(filter));
-  };
-  const filterClearHandler = (): void => {
-    dispatch(clearFilter());
-    setFilter(initialFilter);
-    setIsFilterHidden(true);
+class FilterMenu extends React.Component<IFilterMenuProps, IFilterMenuState> {
+  constructor (props: IFilterMenuProps) {
+    super(props);
+
+    this.setFilter = this.setFilter.bind(this);
+    this.filterButtonHandler = this.filterButtonHandler.bind(this);
+    this.filterClearHandler = this.filterClearHandler.bind(this);
+    this.changeFromInFilter = this.changeFromInFilter.bind(this);
+    this.changeToInFilter = this.changeToInFilter.bind(this);
+
+    this.state = {
+      filter: { from: '', to: '' }
+    };
+  }
+
+  setFilter (value: IFilterOptions): void {
+    this.setState({
+      filter: value
+    });
+  }
+
+  filterButtonHandler (): void {
+    store.dispatch(changeFilter(this.state.filter));
+  }
+
+  filterClearHandler (): void {
+    store.dispatch(clearFilter());
+    this.setFilter({ from: '', to: '' });
+    this.props.setIsFilterHidden(true);
   };
 
-  const changeFromInFilter = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setFilter({ ...filter, from: e.target.value });
+  changeFromInFilter (e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setFilter({ ...this.state.filter, from: e.target.value });
   };
-  const changeToInFilter = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setFilter({ ...filter, from: e.target.value });
+
+  changeToInFilter (e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setFilter({ ...this.state.filter, from: e.target.value });
   };
-  return (
-    <div className={isFilterHidden ? styles.hidden : styles.filter}>
-      <div className={styles.filterContainer}>
-        <div className={styles.filterInput}>
-          <span>с: </span>
-          <input
-            type="date"
-            value={filter.from}
-            onChange={changeFromInFilter}
-          />
+
+  render (): JSX.Element {
+    return (
+      <div className={this.props.isFilterHidden ? styles.hidden : styles.filter}>
+        <div className={styles.filterContainer}>
+          <div className={styles.filterInput}>
+            <span>с: </span>
+            <input
+              type="date"
+              value={this.state.filter.from}
+              onChange={this.changeFromInFilter}
+            />
+          </div>
+          <div className={styles.filterInput}>
+            <span>по: </span>
+            <input
+              type="date"
+              value={this.state.filter.to}
+              onChange={this.changeToInFilter}
+            />
+          </div>
+          <button onClick={this.filterButtonHandler} className={styles.filterButton}>Фильтровать</button>
+          <button onClick={this.filterClearHandler} className={styles.filterRemove}><img src={remove} alt="remove" /></button>
         </div>
-        <div className={styles.filterInput}>
-          <span>по: </span>
-          <input
-            type="date"
-            value={filter.to}
-            onChange={changeToInFilter}
-          />
-        </div>
-        <button onClick={filterButtonHandler} className={styles.filterButton}>Фильтровать</button>
-        <button onClick={filterClearHandler} className={styles.filterRemove}><img src={remove} alt="remove" /></button>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FilterMenu;
