2a82de8b00dbf40f1076d686e8c2e18ad3d0f413
user <kupaev.itrum@gmail.com>
2023-02-15 12:47:16 +0300

    AuthSnackbar done

--- src/components/AuthSnackbar/AuthSnackbar.tsx
+++ src/components/AuthSnackbar/AuthSnackbar.tsx
@@ -1,43 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Alert, Snackbar } from '@mui/material';
 
-import { useAppSelector } from '../../hooks/redux';
-
-import type { FC } from 'react';
 import type { IAuthSnackbar } from '../../interfaces/IAuthSnackbar';
 import { AuthEnum } from '../../interfaces/AuthEnum';
+import { store } from '../../store/store';
 
-const AuthSnackbar: FC<IAuthSnackbar> = ({ setRenderType, renderType }): JSX.Element => {
-  const { regStatus: message, isLoading: isLoadingReg } = useAppSelector(state => state.regReducer);
-  const { error: logMessage, isLoading: isLoadingLogin } = useAppSelector(state => state.loginReducer);
+interface AuthSnackbarState {
+  message: string
+  isLoadingReg: boolean
+  logMessage: string
+  isLoadingLogin: boolean
+  isInfoOpened: boolean
+  customMessage: string
+}
 
-  const [isInfoOpened, setIsInfoOpened] = useState(false);
-  const [customMessage, setCustomMessage] = useState('');
-
-  const closeInfo = (): void => {
-    setIsInfoOpened(false);
+class AuthSnackbar extends React.Component<IAuthSnackbar, AuthSnackbarState> {
+  constructor (props: IAuthSnackbar) {
+    super(props);
+    this.setIsInfoOpened = this.setIsInfoOpened.bind(this);
+    this.setCustomMessage = this.setCustomMessage.bind(this);
+    this.closeInfo = this.closeInfo.bind(this);
+    this.state = {
+      message: store.getState().regReducer.regStatus,
+      isLoadingReg: store.getState().regReducer.isLoading,
+      logMessage: store.getState().loginReducer.error,
+      isLoadingLogin: store.getState().loginReducer.isLoading,
+      isInfoOpened: false,
+      customMessage: ''
+    };
   };
 
-  useEffect(() => {
-    if (!isLoadingReg) {
-      if (message === "User's registration is succesful") {
-        setRenderType(AuthEnum.login);
-        setCustomMessage('Registration is successful. Now you can log in');
-      }
-      if (message !== '') {
-        setIsInfoOpened(true);
-      } else if (logMessage !== '') {
-        setIsInfoOpened(true);
+  setIsInfoOpened (value: boolean): void {
+    this.setState({
+      isInfoOpened: value
+    });
+  };
+
+  setCustomMessage (value: string): void {
+    this.setState({
+      customMessage: value
+    });
+  }
+
+  closeInfo (): void {
+    this.setIsInfoOpened(false);
+  }
+
+  componentDidUpdate (prevProps: IAuthSnackbar, prevState: AuthSnackbarState): void {
+    if (prevState.isLoadingLogin !== this.state.isLoadingLogin || prevState.isLoadingReg !== this.state.isLoadingReg) {
+      if (!this.state.isLoadingReg) {
+        if (this.state.message === "User's registration is succesful") {
+          this.props.setRenderType(AuthEnum.login);
+          this.setCustomMessage('Registration is successful. Now you can log in');
+        }
+        if (this.state.message !== '') {
+          this.setIsInfoOpened(true);
+        } else if (this.state.logMessage !== '') {
+          this.setIsInfoOpened(true);
+        }
       }
     }
-  }, [isLoadingReg, isLoadingLogin]);
-  return (
-    <Snackbar open={isInfoOpened} autoHideDuration={4000} onClose={closeInfo}>
-      <Alert onClose={closeInfo} severity="info" sx={{ width: '100%' }}>
-        {customMessage !== '' ? customMessage : (renderType === AuthEnum.login ? logMessage : message)}
-      </Alert>
-    </Snackbar>
-  );
-};
+  }
+
+  render (): JSX.Element {
+    return (
+      <Snackbar open={this.state.isInfoOpened} autoHideDuration={4000} onClose={this.closeInfo}>
+        <Alert onClose={this.closeInfo} severity="info" sx={{ width: '100%' }}>
+          {this.state.customMessage !== '' ? this.state.customMessage : (this.props.renderType === AuthEnum.login ? this.state.logMessage : this.state.message)}
+        </Alert>
+      </Snackbar>
+    );
+  }
+}
 
 export default AuthSnackbar;
