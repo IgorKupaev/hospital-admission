c25c21893d1fe28df1d73e2fa08603db47926eae
user <kupaev.itrum@gmail.com>
2023-02-15 15:10:32 +0300

    SortMenu done

--- src/components/SortMenu/SortMenu.tsx
+++ src/components/SortMenu/SortMenu.tsx
@@ -1,62 +1,84 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { changeSort } from '../../store/reducers/sortSlice';
-import { useAppDispatch } from '../../hooks/redux';
 
 import styles from './SortMenu.module.scss';
 
 import type { ISortMenu } from '../../interfaces/ISortMenu';
+import { store } from '../../store/store';
 
-const SortMenu = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const [sortOptions, setSortOptions] = useState<ISortMenu>({ sort: 'none', direction: 'increase' });
-  const isDirectionHidden = !!(sortOptions.sort === '' || sortOptions.sort === 'none');
+interface SortMenuState {
+  sortOptions: ISortMenu
+  isDirectionHidden: boolean
+}
 
-  const sortHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+class SortMenu extends React.Component<unknown, SortMenuState> {
+  constructor (props: unknown) {
+    super(props);
+
+    this.setSortOptions = this.setSortOptions.bind(this);
+    this.sortHandler = this.sortHandler.bind(this);
+    this.directionHandler = this.directionHandler.bind(this);
+
+    this.state = {
+      sortOptions: { sort: 'none', direction: 'increase' },
+      isDirectionHidden: true
+    };
+  }
+
+  setSortOptions (value: ISortMenu): void {
+    this.setState({
+      sortOptions: value
+    });
+  }
+
+  sortHandler (e: React.ChangeEvent<HTMLSelectElement>): void {
     if (e.target.value === 'none') {
-      setSortOptions({ direction: 'increase', sort: 'none' });
-      dispatch(changeSort({ direction: 'increase', sort: 'none' }));
+      this.setSortOptions({ direction: 'increase', sort: 'none' });
+      store.dispatch(changeSort({ direction: 'increase', sort: 'none' }));
       return;
     }
-    setSortOptions({ ...sortOptions, sort: e.target.value });
-    dispatch(changeSort({ ...sortOptions, sort: e.target.value }));
-  };
+    this.setSortOptions({ ...this.state.sortOptions, sort: e.target.value });
+    store.dispatch(changeSort({ ...this.state.sortOptions, sort: e.target.value }));
+  }
 
-  const directionHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
-    setSortOptions({ ...sortOptions, direction: e.target.value });
-    dispatch(changeSort({ ...sortOptions, direction: e.target.value }));
-  };
+  directionHandler (e: React.ChangeEvent<HTMLSelectElement>): void {
+    this.setSortOptions({ ...this.state.sortOptions, direction: e.target.value });
+    store.dispatch(changeSort({ ...this.state.sortOptions, direction: e.target.value }));
+  }
 
-  return (
-    <div className={styles.sort}>
-      <div className={styles.sortContainer}>
-      <div className={styles.sortSelect}>
-          <span className={styles.sortText}>Сортировать:</span>
-          <select
-            onChange={sortHandler}
-            value={sortOptions.sort}
-            className={styles.select}
-          >
-            <option value="none">None</option>
-            <option value="pacient">По имени</option>
-            <option value="doctor">По врачу</option>
-            <option value="date">По дате</option>
-          </select>
-        </div>
-        <div className={isDirectionHidden ? styles.hidden : styles.sortDirection}>
-          <span className={styles.sortText}>Направление:</span>
-          <select
-            value={sortOptions.direction}
-            className={styles.select}
-            onChange={directionHandler}
-          >
-            <option value="increase">По возрастанию</option>
-            <option value="decrease">По убыванию</option>
-          </select>
+  render (): JSX.Element {
+    return (
+      <div className={styles.sort}>
+        <div className={styles.sortContainer}>
+        <div className={styles.sortSelect}>
+            <span className={styles.sortText}>Сортировать:</span>
+            <select
+              onChange={this.sortHandler}
+              value={this.state.sortOptions.sort}
+              className={styles.select}
+            >
+              <option value="none">None</option>
+              <option value="pacient">По имени</option>
+              <option value="doctor">По врачу</option>
+              <option value="date">По дате</option>
+            </select>
+          </div>
+          <div className={this.state.sortOptions.sort === '' || this.state.sortOptions.sort === 'none' ? styles.hidden : styles.sortDirection}>
+            <span className={styles.sortText}>Направление:</span>
+            <select
+              value={this.state.sortOptions.direction}
+              className={styles.select}
+              onChange={this.directionHandler}
+            >
+              <option value="increase">По возрастанию</option>
+              <option value="decrease">По убыванию</option>
+            </select>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SortMenu;
